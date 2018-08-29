43ce39f535474c47e9648b7acad3969babfd0e11
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-08-29 20:02:42 -0300

    calculo final

--- 4°-Periodo/Programacao para internet I/imc-calculo/index.php
+++ 4°-Periodo/Programacao para internet I/imc-calculo/index.php
@@ -2,7 +2,6 @@
 <!-- 
 Desenvolver um programa em PHP que calcule 
 o IMC de uma pessoa 
-
 -->
 
 <html>
@@ -13,7 +12,12 @@ o IMC de uma pessoa
 	<!-- Formulario de dados -->
 	<form method="POST"> 
 		Peso: <input type="text" name="peso"><br>
-		Idade: <input type="text" name="idade"><br>
+		altura: <input type="text" name="altura"><br>
+		Sexo:
+		<select>
+			<option>M</option>
+			<option>F</option>
+		</select><br>
 		<input type="submit"  value="enviar">
 	</form>
 	
@@ -35,14 +39,42 @@ function calcula_imc($peso, $altura){
 if( isset($_POST["peso"]) ){
 //declara variavel com peso e altura 
 	$peso 	= $_POST["peso"];
-	$altura = $_POST["idade"];
+	$altura = $_POST["altura"];
 	//pega retorno da função e atribui para a variavel 
 	$resultado = calcula_imc($peso, $altura); 
 	//imprime valor atribuido a variavel
-	echo $resultado;
+	echo $resultado.'<br>';
+
+	//resultado final 
+	if($resultado < 17){ echo"Muito Abaixo do peso";}
+	else if($resultado > 17 and $resultado < 18.50){ echo"Abaixo do peso";}
+	else if($resultado > 18.5 and $resultado < 24.99){ echo"Peso normal";}
+	else if($resultado > 25 and $resultado < 29.99){ echo"Acima do peso";}
+	else if($resultado > 30 and $resultado < 34.99){ echo"Obsidade";}
+	else if($resultado > 35 and $resultado < 39.99){ echo"Obsidade II";}
 }
+
+
 
 //final do php 
 ?>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
