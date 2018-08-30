e04b889d92899aef118b6686f626c7ee4efe2729
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-08-30 14:05:48 -0300

    funcoes homem e mulher

--- 4°-Periodo/Programacao para internet I/imc-calculo/index.php
+++ 4°-Periodo/Programacao para internet I/imc-calculo/index.php
@@ -36,15 +36,7 @@ function calcula_imc($peso, $altura){
 
 }
 
-if( isset($_POST["peso"]) ){
-//declara variavel com peso e altura 
-	$peso 	= $_POST["peso"];
-	$altura = $_POST["altura"];
-	//pega retorno da função e atribui para a variavel 
-	$resultado = calcula_imc($peso, $altura); 
-	//imprime valor atribuido a variavel
-	echo $resultado.'<br>';
-
+function imc_masculino($resultado){
 	//resultado final 
 	if($resultado < 17){ echo"Muito Abaixo do peso";}
 	else if($resultado > 17 and $resultado < 18.50){ echo"Abaixo do peso";}
@@ -54,8 +46,38 @@ if( isset($_POST["peso"]) ){
 	else if($resultado > 35 and $resultado < 39.99){ echo"Obsidade II";}
 }
 
+function imc_feminino($resultado){
+	//resultado final 
+	if($resultado < 17){ echo"Muito Abaixo do peso";}
+	else if($resultado > 17 and $resultado < 18.50){ echo"Abaixo do peso";}
+	else if($resultado > 18.5 and $resultado < 24.99){ echo"Peso normal";}
+	else if($resultado > 25 and $resultado < 29.99){ echo"Acima do peso";}
+	else if($resultado > 30 and $resultado < 34.99){ echo"Obsidade";}
+	else if($resultado > 35 and $resultado < 39.99){ echo"Obsidade II";}	
+}
 
+if( isset($_POST["peso"]) ){
+//declara variavel com peso e altura 
+	$peso 	= $_POST["peso"];
+	$altura = $_POST["altura"];
+	$sexo   = 1;
+	//pega retorno da função e atribui para a variavel 
+	$resultado = calcula_imc($peso, $altura); 
+	//imprime valor atribuido a variavel
+	echo $resultado.'<br>';
 
+	//caso sexo seja m axecuta case se nao defalt
+	switch ($sexo){
+		//maculino
+		case 1:
+			imc_masculino($resultado);
+		break;
+		//feminino
+		default:
+			imc_feminino($resultado);
+		break;
+	}
+}
 //final do php 
 ?>
 </body>
