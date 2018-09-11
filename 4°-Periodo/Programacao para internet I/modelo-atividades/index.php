b7e49147f674f94255a9963f8816e02c6ef1217c
dieisson <dieisson.martins.santos@gmail.com>
2018-09-11 15:03:11 -0300

    criado modelo para os proximos exercicios

---
+++ 4°-Periodo/Programacao para internet I/modelo-atividades/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Nome atividade </title>
+	<meta charset="utf-8">
+
+	<!-- estilo-->
+	<style type="text/css">
+		.container-pequeno {
+    		margin: 0 auto;
+   		 	width: 400px;
+   		 }
+   		 .container-medio {
+    		margin: 0 auto;
+   		 	width: 600px;
+   		 }
+   		 .container-largo {
+    		margin: 0 auto;
+   		 	width: 800px;
+   		 }
+   		 .caixa{
+   		 	padding: 1%;
+    		border: 1px #e2e2e2 solid;
+    		background: #eaeaea;
+    		color: #6d6262;
+   		 }
+   		form{
+   			width: 100%;
+   		}
+   		input{
+   			padding: 3%;
+    		width: 100%;
+    		border: none;
+    		margin: 1%;
+    		background: #f3f3f3;
+    		border-radius: 1%;
+   		}
+   		input[type="submit"]{
+   			background: #ff8282;
+    		color: #fff;
+    		font-weight: bold;
+    		width: 105.8%;
+   		}
+	</style>
+
+	<!-- interação -->
+	<script type="text/javascript">
+		
+	</script>
+
+</head>
+
+<!-- 
+descrição da atividade
+-->
+
+<!-- linguadem de programação PHP -->
+<?php
+
+?>
+
+<!-- corpo da pagina -->
+<body>
+
+	<div class="container-largo caixa">
+		<p>fineza deixar mensagem no portal que a reunião da Câmara Municipal será excepcionalmente realizada nesta quinta -feira com a seguinte pauta:</p>
+	</div>
+
+
+	<div class="container-pequeno">
+		<form method="POST">
+			<input type="text" name="texto" placeholder="digite texto">
+			<input type="submit" value="Enviar">
+		</form>
+	</div>
+
+</body>
+</html>
