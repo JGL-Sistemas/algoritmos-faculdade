a71a7f0e5f013cf53d0243cf06ef4f51e30aa889
dieisson <dieisson.martins.santos@gmail.com>
2018-09-26 11:57:26 -0300

    add font aswome e mudança do formulario

--- 4°-Periodo/Programacao para internet I/crud-php-1/header.php
+++ 4°-Periodo/Programacao para internet I/crud-php-1/header.php
@@ -4,5 +4,6 @@
 	<title>cadastro pessoa</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+	<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 </head>
 <body>
--- 4°-Periodo/Programacao para internet I/crud-php-1/index.php
+++ 4°-Periodo/Programacao para internet I/crud-php-1/index.php
@@ -9,29 +9,50 @@
 		$cpf 		= $_POST['cpf'];
 		$ci 		= $_POST['ci'];
 	
-	getPessoa($nome, $cpf, $ci, $conexao);
+	//getPessoa($nome, $cpf, $ci, $conexao);
 	}
 	?>
 
-	<div class="container-pequeno">
-		<form method="POST">
-			<span>Nome:</span>
-			<input type="text" name="nome" placeholder="digite nome"><br/>
+<div class="container-grande">
+		<h2>Matriculados</h2>
+		<p>Todos os alunos matriculados</p>
+		<table>
+		  <tr>
+		    <th>Nome</th>
+		    <th>Matricula</th>
+		    <th>Identidade</th>
+		    <th>Nacimento</th>
+		    <th>Editar</th>
+		    <th>Excluir</th>
+		  </tr>
+		  <?php
+		  $sql = "SELECT * FROM pessoa";
+		  $resultado = mysqli_query($conexao, $sql);
 
-			<span>Cpf:</span>
-			<input type="text" name="cpf" placeholder="digite cpf"><br/>
+		  	if (mysqli_num_rows($resultado) > 0) {
+		    	while($row = mysqli_fetch_assoc($resultado)) { ?>
+		  			<tr>
+		  			  <td><?php echo $row["nome"] ?></td>
+		  			  <td><?php echo $row["cpf"] ?></td>
+		  			  <td><?php echo $row["ci"] ?></td>
+		  			  <td ><?php echo $row["nascimento"] ?></td>
+		  			  <td><a href="#"><i class="fa fa-pencil" aria-hidden="true"></i></a></td>
+		  			  <td><a href="#"><i class="fa fa-times" aria-hidden="true"></i></a></td>
+		  			</tr>
+				<?php }
+			} ?>
+				<tr>
+					<form method="POST">
+		  		  		<td><input type="text" name="nome" placeholder="Digite nome"></td>
+		  		  		<td><input type="text" name="cpf" placeholder="Digite cpf"><br/></td>
+		  		  		<td><input type="text" name="ci" placeholder="Digite identidade"><br/></td>
+		  		  		<td><input type="text" name="nacimento" placeholder="Digite nacimento"><br/></td>
+		  		  		<td><input type="submit" value="Adicionar Novo"></td>
 
-			<span>Identidade:</span>
-			<input type="text" name="ci" placeholder="digite identidade"><br/>
-
-			<!--<span>Nascimento:</span>
-			<input type="date" name="nascimento"><br/>-->
-
-			<input type="submit" value="Enviar">
-		</form>
+					</form>
+		  		</tr>
+		</table>		
 	</div>
-
-<?php ImprimiPessoa($conexao);?>
 
 </body>
 </html>
