61216fa98404deffde92fc7e8a8894d94ef71fcd
dieisson <dieisson.martins.santos@gmail.com>
2018-11-08 09:08:30 -0200

    edição no ope.php

--- 4°-Periodo/Programacao para internet I/projeto-final-periodo/session/ope.php
+++ 4°-Periodo/Programacao para internet I/projeto-final-periodo/session/ope.php
@@ -1,25 +1,27 @@
 <?php 
 session_start();
 
-$servername = "localhost";
-$username 	= "root";
-$password 	= "";
+//$servername = "localhost";
+//$username 	= "root";
+//$password 	= "";
+//
+//$conn = mysqli_connect($servername, $username, $password);
+//$select = mysqli_select_db("sistema") or die("Sem acesso ao DB, Entre em contato com o Administrador, gilson_sales@bytecode.com.br");
 
-$conn = mysqli_connect($servername, $username, $password);
-$select = mysql_select_db("server") or die("Sem acesso ao DB, Entre em contato com o Administrador, gilson_sales@bytecode.com.br");
-
-if (!$conn){
-    die("Falha na conexão: " . mysqli_connect_error());
-}
-echo "Conectado";
+//if (!$conn){
+//    die("Falha na conexão: " . mysqli_connect_error());
+//}
 
 
 $login = $_POST['login'];
 $senha = $_POST['senha'];
+$l = 'dieisson';
+$s = '1234';
+echo "Conectado";
  
-$result = mysql_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
+//$result = mysqli_query("SELECT * FROM `USUARIO` WHERE `NOME` = '$login' AND `SENHA`= '$senha'");
 
-if(mysql_num_rows ($result) > 0 ){
+if(/*mysqli_num_rows ($result) > 0 */ $login == $l and $senha == $s ){
 	$_SESSION['login'] = $login;
 	$_SESSION['senha'] = $senha;
 	header('location:site.php');
