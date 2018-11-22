3bd5a632c4326032f6e54967349f8fc18e9379c0
dieisson <dieisson.martins.santos@gmail.com>
2018-11-22 10:15:28 -0200

    sessions

---
+++ 4°-Periodo/session-function/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+
+echo $_SESSION['nome'];
+?>
+</br>
+<a href="session.php">::session start::</a>
+</br>
+<a href="destroy.php">::session destroy::</a>
---
+++ 4°-Periodo/session-function/session.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+
+$_SESSION['nome'] = 'dieisson martins';
+
+header('Location:index.php');
+
+
+?>
