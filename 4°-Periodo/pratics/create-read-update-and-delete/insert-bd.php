f86765fea968ab8adc95c28e85a04b4fb9d48df4
dieisson <dieisson.martins.santos@gmail.com>
2018-11-23 10:08:43 -0200

    crud functions php mysql

---
+++ 4°-Periodo/pratics/create-read-update-and-delete/insert-bd.php
@@ -0,0 +1,26 @@
+<?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO pessoa (firstname, lastname, email)
+VALUES ('Dieisson', 'Martins dos Santos', 'dieisson.martins.santos@gmail.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
