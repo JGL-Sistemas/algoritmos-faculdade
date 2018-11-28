3d6e6197a3732328c3bfecde3b131f546a84cf57
dieisson <dieisson.martins.santos@gmail.com>
2018-11-28 11:34:12 -0200

    funcão excluir curso

--- 4°-Periodo/Programacao para internet I/POO/curso.class.php
+++ 4°-Periodo/Programacao para internet I/POO/curso.class.php
@@ -5,35 +5,64 @@ class Curso{
     //atributos
     private $codigo = array();
     private $nome   = array();
-    private $id;
+    public $id;
     
     public function setTurma(array $turmas,$i){
         $this->id = $i;
         $this->codigo[$this->id] = $turmas[$this->id]['codigo']; 
         $this->nome[$this->id] = $turmas[$this->id]['nome']; 
     }
+    public function getCodigo(){
+        return $this->codigo[$this->id];
+    }
     public function getNome(){
         return $this->nome[$this->id];
+    }
+    public function mostrar(){
+        for ($i=0; $i <count($this->codigo); $i++){
+            $this->id = $i;
+            if($this->getCodigo() != NULL and $this->getNome() != NULL){
+                echo $this->getCodigo().'-';
+                echo $this->getNome().'<hr>';    
+            }              
+        }
+    }
+    public function excluir($id){
+        for ($i=0; $i <count($this->codigo); $i++){
+            if($id == $i){
+                 $this->codigo[$i] = NULL;
+                 $this->nome[$i] = NULL;
+             }
+        }
     } 
 }
-
 $turma = new Curso;
-
 $turmas = array(
-    array('codigo'=>'1','nome'=>'Sistemas de Informação </br>'),
-    array('codigo'=>'2','nome'=>'Direito </br>'),
-    array('codigo'=>'3','nome'=>'Pedagogia </br>'),
-    array('codigo'=>'4','nome'=>'Pedagogia2 </br>'),
+    array('codigo'=>'1','nome'=>'Sistemas de Informação'),
+    array('codigo'=>'2','nome'=>'Direito'),
+    array('codigo'=>'3','nome'=>'Pedagogia'),
+    array('codigo'=>'4','nome'=>'Direito'),
+    array('codigo'=>'5','nome'=>'Administração'),
+    array('codigo'=>'6','nome'=>'Engenharia Civil'),
+    array('codigo'=>'7','nome'=>'Ciências Contábeis'),
+    array('codigo'=>'8','nome'=>'Ciência da Computação'),
+    array('codigo'=>'9','nome'=>'Engenharia de Produção'),
+    array('codigo'=>'10','nome'=>'Engenharia Mecânica'),
+    array('codigo'=>'11','nome'=>'Educação Física'),
+    array('codigo'=>'12','nome'=>'Engenharia Elétrica'),
+    array('codigo'=>'13','nome'=>'Gestão Logística')
 );
 
 $qtd = count($turmas);
-
-
 for($i=0; $i<$qtd; $i++){ 
-    $turma->setTurma($turmas,$i);
-
-   // echo $turma->getCodigo();
-   echo $turma->getNome();
+   $turma->setTurma($turmas,$i);
 }
+
+$turma->excluir(1);
+$turma->excluir(12);
+
+
+
+$turma->mostrar();
 
 
