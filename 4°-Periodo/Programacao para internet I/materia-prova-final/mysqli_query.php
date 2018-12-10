b482f220aaf127beca6c98938b638a81e4deff23
dieisson <dieisson.martins.santos@gmail.com>
2018-12-10 14:11:26 -0200

    comentarios explicativos

--- 4°-Periodo/Programacao para internet I/materia-prova-final/mysqli_query.php
+++ 4°-Periodo/Programacao para internet I/materia-prova-final/mysqli_query.php
@@ -1,11 +1,13 @@
 <?php
 
+//importa script de outro arquivo
 require_once('mysqli_connect.php');
 
 
-
+//inserir no banco de dados 
 $sql = "INSERT INTO `curso` (`cod_curso`, `nome`, `sigla`, `periodo`) VALUES (NULL, '$nome', '$sigla','$periodo')";
 
+//inseri dados no banco  
 $inserir = mysqli_query($conexao,$sql);
 
 
--- 4°-Periodo/Programacao para internet I/materia-prova-final/poo.php
+++ 4°-Periodo/Programacao para internet I/materia-prova-final/poo.php
@@ -1,13 +1,7 @@
-
 <?
-
-//delimitador de acesso: private, public
-
-
-
+//Métodos mágicos em PHP são métodos previamente definidos de uma classe que tem funcionalidades específicas, que são invocados quando trabalhamos com as propriedades e métodos de um objeto (instância de uma classe).
 public function _construct() { //encapsulamento
  
-//Métodos mágicos em PHP são métodos previamente definidos de uma classe que tem funcionalidades específicas, que são invocados quando trabalhamos com as propriedades e métodos de um objeto (instância de uma classe).
 
 //Com eles você pode fazer interceptações ao longo da execução do script e modificar o curso da aplicação, caso necessário.
 }
@@ -16,6 +10,8 @@ public function _construct() { //encapsulamento
 class Produto{
 
 	
+	//delimitador de acesso: private, public
+			//atributo caracteristica
 	private $descricao;
 	private $preco;
 
@@ -24,26 +20,26 @@ class Produto{
 		$this->preco = $preco;
 	}
 
-	
+	//metodos ações para um objeto pega valor 
 	public function setDescricao($valor){
 		$this->descricao = $valor;
 	}
 
-	
+	//metodos ações para um objeto
 	public function setPreco($valor){
 		$this->preco = $valor;
 	}
 
-	
+	//metodos ações para um objeto retorna valor
 	public function getDescricao(){
 		return $this->descricao;
 	}
-
+	//metodos ações para um objeto
 	public function getPreco(){
 		return $this->preco;
 	}
 
-	
+	//metodos ações para um objeto
 	public function getDetalhes(){
 		return "O produto {$this->descricao} custa {$this->preco} reais";
 	}
@@ -61,6 +57,40 @@ $p1 = new Produto('livro', 50);
 
 //var_dump($p1);
 echo $p1->getDetalhes();
+
+//-------------------------
+
+//Método get
+//Quando formos acessar, “pegar” alguns atributos da classe, devemos utilizar os métodos GET. Esse método sempre retornará um valor, seja ele String, int ,double etc. Então devemos criar uma função.
+
+//--------------------------
+//Método set
+//ara alterarmos, modificarmos os valores de um atributo da classe de maneira protegida, utilizamos os métodos SET. Esse método não terá um retorno, pois o atributo será somente modificado, criando um método de tipo VOID, sem retorno. Porém ele deve receber algum argumento para que possa ocorrer a devida alteração. 
+
+//-----------------------------
+//HERANÇA
+//A herança representa uma das principais características da Orientação a Objetos, até porque, somos capazes de implementar tipos de dados hierarquicamente. Através do conceito de herança, conseguimos implementar classes de uso geral, que possuam características comuns a várias entidades relacionadas.
+
+//Essas classes poderão ser estendidas por outras, produzindo assim, classes mais especializadas e, que implementem funcionalidades que as tornam únicas.
+
+//----------------------------------
+class Pai()
+{
+
+}
+
+class Filho extends Pai
+{
+    #a classe filha utiliza a instrução `extends` e em seguida define
+    # o nome da classe que está sendo estendida
+}
+
+
+
+
+
+
+
 
 
 
